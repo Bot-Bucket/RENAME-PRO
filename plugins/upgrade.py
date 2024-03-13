c44fa84cb419f3556479b7d48a94bfbd110eb6ef
Bot-Bucket <163278950+Bot-Bucket@users.noreply.github.com>
2024-03-13 17:05:03 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -25,9 +25,9 @@ async def upgrade(bot,update):
 	After Payment Send Screenshots Of 
         Payment To Admin @Kirodewal"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("Admin",url = "https://t.me/kirodewal")], 
-        			[InlineKeyboardButton("Phone Pay",url = "https://upayme.vercel.app/Hxbots@sbi"),
-        			InlineKeyboardButton("Paytm Wallet/UPI",url = "https://upayme.vercel.app/Hxbots@sbi")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("Admin",url = "https://t.me/Pirate_King_Luffy_Bot")], 
+        			[InlineKeyboardButton("Phone Pay",url = "https://t.me/Pirate_King_Luffy_Bot"),
+        			InlineKeyboardButton("Paytm Wallet/UPI",url = "https://t.me/Pirate_King_Luffy_Bot")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
@@ -55,9 +55,9 @@ async def upgradecm(bot,message):
 	After Payment Send Screenshots Of 
         Payment To Admin @Kirodewal"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("Admin",url = "https://t.me/kirodewal")], 
-        			[InlineKeyboardButton("Phone Pay",url = "https://upayme.vercel.app/Hxbots@sbi"),
-        			InlineKeyboardButton("Paytm Wallet/UPI",url = "https://upayme.vercel.app/Hxbots@sbi")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("Admin",url = "https://t.me/Pirate_King_Luffy_Bot")], 
+        			[InlineKeyboardButton("Phone Pay",url = "https://t.me/Pirate_King_Luffy_Bot"),
+        			InlineKeyboardButton("Paytm Wallet/UPI",url = "https://t.me/Pirate_King_Luffy_Bot")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await message.reply_text(text = text,reply_markup = keybord)
 
 
